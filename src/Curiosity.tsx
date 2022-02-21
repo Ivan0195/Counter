50a474d973e6eaa3971ff1e0510ad4520903877e
Ivan0195 <iihnatsenka95@gmail.com>
2022-02-21 16:49:27 +0300

    refactoredPamping

--- src/Curiosity.tsx
+++ src/Curiosity.tsx
@@ -6,40 +6,11 @@ type CuriosityPropsType = {
     onClickHandler: ()=>void
 }
 
+const messages = ['Не нажимать!', 'Не надо!', 'Ну я же просил!', 'Зачем ты это делаешь?!', 'Ты хочешь позлить меня?!', 'Советую прекратить!', 'Я начинаю терять терперие!',
+'Ты играешь с огнем!', '3...', '2...', '1...']
+
 const Curiosity = (props:CuriosityPropsType) => {
-    if (props.curiosityLevel===0) {
-        return <div className={'curiosity'} onClick={props.onClickHandler}>Не нажимать!</div>
-    }
-    if (props.curiosityLevel===1) {
-        return <div className={'curiosity'} onClick={props.onClickHandler}>Не надо!</div>
-    }
-    if (props.curiosityLevel===2) {
-        return <div className={'curiosity'} onClick={props.onClickHandler}>Ну я же просил!</div>
-    }
-    if (props.curiosityLevel===3) {
-        return <div className={'curiosity'} onClick={props.onClickHandler}>Зачем ты это делаешь?!</div>
-    }
-    if (props.curiosityLevel===4) {
-        return <div className={'curiosity'} onClick={props.onClickHandler}>Ты хочешь позлить меня?!</div>
-    }
-    if (props.curiosityLevel===5) {
-        return <div className={'curiosity'} onClick={props.onClickHandler}>Советую прекратить!</div>
-    }
-    if (props.curiosityLevel===6) {
-        return <div className={'curiosity'} onClick={props.onClickHandler}>Я начинаю терять терперие!</div>
-    }
-    if (props.curiosityLevel===7) {
-        return <div className={'curiosity'} onClick={props.onClickHandler}>Ты играешь с огнем!</div>
-    }
-    if (props.curiosityLevel===8) {
-        return <div className={'curiosity'} onClick={props.onClickHandler}>3...</div>
-    }
-    if (props.curiosityLevel===9) {
-        return <div className={'curiosity'} onClick={props.onClickHandler}>2...</div>
-    }
-    if (props.curiosityLevel===10) {
-        return <div className={'curiosity'} onClick={props.onClickHandler}>1...</div>
-    }else {return <div>{null}</div>}
-};
+    return <div className={'curiosity'} onClick={props.onClickHandler}>{messages[props.curiosityLevel]}</div>
+}
 
 export default Curiosity;
